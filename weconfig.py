70803c95e09d03eb333b2d9da8976cb611e31b80
rhchen99 <rec004@ucsd.edu>
2025-10-23 12:48:07 -0700

    updated wedriver

--- weconfig.py
+++ weconfig.py
@@ -1,26 +1,60 @@
+'''
+This is the configuration file for the WearEChem project.
+Renhe Chen (rec004@ucsd.edu)
+University of California San Diego
+'''
 #DAC CONFIG
-VREF = 2560 #reference voltage for DAC (mV)
+VREF = 2560             #reference voltage for DAC (mV)
+
+CGM_EXT = 0             # 0 to enable constant gm internal refrence, 1 to use external
 
 #ADC CONFIG
-ADC_OTA1 = 0
-ADC_OTA2 = 0
-ADC_STARTUP_SEL = 0
-ADC_C2 = 0
-ADC_MUX = 0
+ADC_OTA1 = 1            # from 0 to 2 (will be thermo encoded)
+ADC_OTA2 = 1            # from 0 to 2 (will be thermo encoded)
+ADC_STARTUP_SEL = 0     # from 0 to 3
+ADC_C2 = 2              # from 0 to 4 (will be thermo encoded)
 
 #PSTAT CONFIG
-PSTAT_S_BIAS = 0
-PSTAT_S_CC = 0
-PSTAT_S_OTAW = 0
-PSTAT_S_CLSABW = 0
-PSTAT_S_OTAR = 0
-PSTAT_S_CLSABR = 0
-PSTAT_S_SRE = 0
+#below are single-bit switches to enable/disable PSTAT blocks
+PSTAT_S_BIAS = 0        # bias
+PSTAT_S_CC = 0          # current conveyor
+PSTAT_S_OTAW = 0        # working electrode OTA
+PSTAT_S_CLSABW = 0      # working electrode second stage
+PSTAT_S_OTAR = 0        # reference electrode OTA
+PSTAT_S_CLSABR = 0      # reference electrode second stage
+PSTAT_S_SRE = 0         # slew rate enhancement
+
+#2x current switch for PSTAT blocks
 PSTAT_OTAWI2X = 0
 PSTAT_OTARI2X = 0
 PSTAT_CLSABWI2X = 0
 PSTAT_CLSABRI2X = 0
 
 #CC CONFIG
-CC_GAIN = 1 #set CC gain. 0-> 10x, 1-> 1x, 2-> 0.1x
-CC_SEL = 5
+CC_GAIN = 1             # set CC gain. 0 -> 10x, 1 -> 1x, 2 -> 0.1x
+CC_SEL = 5              # select CC device, range from 1 to 11. default is 5
+
+#mode definition
+MODE_DEFAULT = 0
+MODE_PM = 1
+MODE_PSTAT = 2
+MODE_ADC = 3
+MODE_ION = 4
+
+#Opal Kelly Endpoint Configuration
+
+#FPGA RST ALL Wire
+RST_ALL = 0x10         # reset all endpoint
+
+#FPGA SPI CONFIG
+SPI_TRIG = 0x40        # spi trigger endpoint
+SPI_IN = 0x80          # spi input buffer endpoint (pipein)
+SPI_OUT = 0xA0         # spi output buffer endpoint (pipeout)
+
+#FPGA ADC CONFIG
+ADC_TRIG = 0x60         # adc trigger endpoint
+ADC_OUT = 0xB0          # adc output buffer endpoint (pipeout)
+
+#FPGA DAC CONFIG
+DAC_TRIG = 0x70         # dac trigger endpoint
+DAC_IN = 0x90           # dac input buffer endpoint (pipein)
--- wedriver.py
+++ wedriver.py
@@ -1,42 +1,13 @@
-import struct
+'''
+This is the driver module for the WearEChem project.
+Renhe Chen (rec004@ucsd.edu)
+University of California San Diego
+'''
+import time
 import weconfig
+import ok
 
-def analog_to_binary(vin,vref):
-    #convert analog input to 10-bit binary code
-    code = int((vin / vref) * 1024)
-    #expand to 16-bit
-    code = code << 6
-    return code
-
-def binary_to_one_hot(bin,num):
-    one_hot = 1
-    one_hot = one_hot << (bin-1)
-    return one_hot
-
-def gen_config_code():
-    '''
-        generate 36-bit config code to be sent to SPI0
-        with settings from weconfig.py
-    '''
-    code = binary_to_one_hot(weconfig.CC_SEL,11)
-    code = code + (weconfig.CC_GAIN << 11)
-    code = code + (weconfig.PSTAT_CLSABRI2X << 13)
-    code = code + (weconfig.PSTAT_CLSABWI2X << 14)
-    code = code + (weconfig.PSTAT_OTARI2X << 15)
-    code = code + (weconfig.PSTAT_OTAWI2X << 16)
-    code = code + (weconfig.PSTAT_S_SRE << 17)
-    code = code + (weconfig.PSTAT_S_CLSABR << 18)
-    code = code + (weconfig.PSTAT_S_OTAR << 19)
-    code = code + (weconfig.PSTAT_S_CLSABW << 20)
-    code = code + (weconfig.PSTAT_S_OTAW << 21)
-    code = code + (weconfig.PSTAT_S_CC << 22)
-    code = code + (weconfig.PSTAT_S_BIAS << 23)
-    code = code + (weconfig.ADC_MUX << 24)
-    code = code + (weconfig.ADC_C2 << 26)
-    code = code + (weconfig.ADC_STARTUP_SEL << 30)
-    code = code + (weconfig.ADC_OTA2 << 32)
-    code = code + (weconfig.ADC_OTA1 << 34)
-    return code
+dev = ok.okCFrontPanel()
 
 def gen_ramp(vstart,vstop,vstep):
     '''
@@ -90,8 +61,133 @@ def gen_dpv(vstart,vstop,vstep,vpulse):
         data.append(bin)
     return data
 
-def config_timing(teq,t1,t2):
-    return 1
+def spi_write(data):
+    '''
+    Write data to SPI interface and read the response.
+    '''
+    datain = 0
+    # Pack data into bytes
+    dataout = data.to_bytes(5, 'little')
+    # Load data to SPI input buffer
+    dev.WriteToPipeIn(weconfig.SPI_IN, dataout)
+    # Trigger SPI to send data into DUT
+    dev.ActivateTriggerIn(weconfig.SPI_TRIG, 0)
+    time.sleep(0.1)
+    # Trigger SPI again to pop out data
+    dev.ActivateTriggerIn(weconfig.SPI_TRIG, 0)
+    time.sleep(0.1)
+    # Read back from SPI output buffer
+    datain = dev.ReadFromPipeOut(weconfig.SPI_OUT, 5)
+    # Print the data read from SPI
+    print(f'{datain} has been written to SPI 0')
+
+def fpga_init(bitfile):
+    '''
+    Initialize the FPGA device.
+    '''
+    dev.OpenBySerial("")
+    dev.ResetFPGA()
+    error = dev.ConfigureFPGA(bitfile)
+    if error == 0:
+        print("FPGA initialization complete.")
+    else:
+        print(f"FPGA initialization failed with error code: {error}")
+    enable = dev.IsFrontPanelEnabled()
+    if enable:
+        print("FrontPanel is enabled.")
+    else:
+        print("FrontPanel is not enabled.")
+    #reset fpga
+    dev.SetWireInValue(0x00,1)
+    dev.UpdateWireIns()
+    dev.SetWireInValue(0x00,0)
+    dev.UpdateWireIns()
+    print("FPGA initialization complete.")
+
+def system_reset():
+    '''
+    Reset the system to its default state.
+    '''
+    #set RESET_ALL to 1
+    dev.SetWireInValue(weconfig.RST_ALL, 1)
+    dev.UpdateWireIns()
+    time.sleep(0.1)
+    #set RESET_ALL to 0
+    dev.SetWireInValue(weconfig.RST_ALL, 0)
+    dev.UpdateWireIns()
+    print("System reset complete.")
+
+def system_config(mode):
+    '''
+    Configure the system based on the selected mode.
+    mode: see 'mode definition' in 'weconfig.py'
+    '''
+    I_MUX_OUT = 0
+    ION_EN = 0
+    PM_EN = 0
+    ADC_MUX = 0
+    if mode == weconfig.MODE_PM:
+        PM_EN = 1
+    elif mode == weconfig.MODE_PSTAT:
+        ADC_MUX = 2
+    elif mode == weconfig.MODE_ADC:
+        I_MUX_OUT = 1
+        ADC_MUX = 3
+    elif mode == weconfig.MODE_ION:
+        ION_EN = 1
+        ADC_MUX = 1
+    #generate 40-bit config data
+    data = gen_config_code(I_MUX_OUT, ION_EN, PM_EN, ADC_MUX)
+    #send data to SPI
+    spi_write(data)
+
+def gen_config_code(I_MUX_OUT,ION_EN,PM_EN,ADC_MUX):
+    '''
+        assemble 40-bit config code to be sent to SPI0
+        with settings from weconfig.py
+    '''
+    code = binary_to_one_hot(weconfig.CC_SEL,11)
+    code = code + (weconfig.CC_GAIN << 11)
+    code = code + (weconfig.PSTAT_CLSABRI2X << 13)
+    code = code + (weconfig.PSTAT_CLSABWI2X << 14)
+    code = code + (weconfig.PSTAT_OTARI2X << 15)
+    code = code + (weconfig.PSTAT_OTAWI2X << 16)
+    code = code + (weconfig.PSTAT_S_SRE << 17)
+    code = code + (weconfig.PSTAT_S_CLSABR << 18)
+    code = code + (weconfig.PSTAT_S_OTAR << 19)
+    code = code + (weconfig.PSTAT_S_CLSABW << 20)
+    code = code + (weconfig.PSTAT_S_OTAW << 21)
+    code = code + (weconfig.PSTAT_S_CC << 22)
+    code = code + (weconfig.PSTAT_S_BIAS << 23)
+    code = code + (ADC_MUX << 24)
+    code = code + (binary_to_thermo(weconfig.ADC_C2) << 26)
+    code = code + (weconfig.ADC_STARTUP_SEL << 30)
+    code = code + (binary_to_thermo(weconfig.ADC_OTA2) << 32)
+    code = code + (binary_to_thermo(weconfig.ADC_OTA1) << 34)
+    code = code + (PM_EN << 35)
+    code = code + (ION_EN << 36)
+    code = code + (weconfig.CGM_EXT << 37)
+    code = code + (I_MUX_OUT << 38)
+    return code
+
+def analog_to_binary(vin,vref):
+    #convert analog input to 10-bit binary code
+    code = int((vin / vref) * 1024)
+    #expand to 16-bit
+    code = code << 6
+    return code
+
+def binary_to_one_hot(bin,num):
+    one_hot = 1
+    one_hot = one_hot << (bin-1)
+    return one_hot
+
+def binary_to_thermo(bin):
+    thermo = 0
+    for i in range(bin):
+        thermo = thermo << 1
+        thermo = thermo + 1
+    return thermo
 
 
 if __name__ == '__main__':
@@ -99,11 +195,6 @@ if __name__ == '__main__':
     #data = gen_ramp(200,100,-5)
     #data = gen_cv(100,200,100,5)
     #data = gen_dpv(1500,2000,5,100)
-    # Pack data into bytes (little-endian 16-bit)
-    #data_bytes = b''.join(struct.pack('<H', x) for x in data)
-    
-    data = gen_config_code()
-    data = bin(data)[2:]
-    data = data.zfill(36)
-
-    print(f"{data}")
+    data = gen_config_code(0,0,0,0)
+    dataout = data.to_bytes(5, 'little')
+    print(f"{dataout}")
